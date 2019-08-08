361c01abf0678e3456bb5460dddb9fcb7e020ec4
tairaperrault <tairaperrault@gmail.com>
2019-08-08 13:39:14 -0400

    CSS Fourteenth

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /* ==========================================================================
-   Navigation
+   Contact Modal
    ========================================================================== */
+   var modal = document.querySelector("#modal");
+   var modalWrapper = document.querySelector("#modal-wrapper");
+   var buttonClose = document.querySelector("#button-close");
+   var buttonOpen = document.querySelector("#button-open");
+
+   buttonClose.addEventListener("click", function() {
+     modal.classList.toggle("closed");
+     modalWrapper.classList.toggle("closed");
+   });
+
+   buttonOpen.addEventListener("click", function() {
+     modal.classList.toggle("closed");
+     modalWrapper.classList.toggle("closed");
+   });
