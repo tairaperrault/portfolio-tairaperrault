505bb11ea1fba44c6d1c12f92cceedd8acdc33f6
tairaperrault <tairaperrault@gmail.com>
2019-11-03 16:18:26 -0500

    Grid 1

--- js/main.js
+++ js/main.js
@@ -54,3 +54,42 @@
         lightbox.classList.toggle("closed");
         lightboxWrapper.classList.toggle("closed");
       });
+
+
+
+            function resizeGridItem(item){
+               grid = document.getElementsByClassName("grid")[0];
+               rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+               rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+               rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+               item.style.gridRowEnd = "span "+rowSpan;
+            }
+
+            function resizeAllGridItems(){
+               allItems = document.getElementsByClassName("item");
+               for(x=0;x<allItems.length;x++){
+                  resizeGridItem(allItems[x]);
+               }
+            }
+
+            window.onload = resizeAllGridItems();
+
+            window.addEventListener("resize", resizeAllGridItems);
+
+            allItems = document.getElementsByClassName("item");
+            for(x=0;x<allItems.length;x++){
+               imagesLoaded( allItems[x], resizeInstance);
+            }
+
+            function resizeInstance(instance){
+               item = instance.elements[0];
+               resizeGridItem(item);
+            }
+
+            // with jQuery
+            var $container = $('#container');
+
+            // initialize Masonry after all images have loaded
+            $container.imagesLoaded( function() {
+                 $container.masonry();
+            });
